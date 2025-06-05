85e2323cd72ee60a711632b29a22539e1bd765f8
jiatongcherry <jiatongluna@icloud.com>
2025-06-05 17:38:14 +0800

    Delete my-app/app directory

--- my-app/app/api/hello/route.ts
+++
@@ -1,7 +0,0 @@
-
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-  //const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  return NextResponse.json({"message": "Hello, World!"});
-}
--- my-app/app/blog/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = ({params}: {params: {id: string}}) => {
-  return (
-    <div>id: {params.id}</div>
-  )
-}
-
-export default page
