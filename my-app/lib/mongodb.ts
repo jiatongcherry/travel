2a86fefcb70bcf63fa2df66075c5fcf064811ae4
jiatongcherry <jiatongluna@icloud.com>
2025-06-05 17:40:09 +0800

    Update mongodb.ts

--- my-app/lib/mongodb.ts
+++ my-app/lib/mongodb.ts
@@ -13,21 +13,3 @@ if (!cached) {
   cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
-async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
-
-export default connectDB;
